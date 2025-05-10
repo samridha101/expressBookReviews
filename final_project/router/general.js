03dfeb2a9867d3736da387c74229ebd9db88d2f1
samridha101 <samridhasuresh5@gmail.com>
2025-05-10 16:32:25 -0400

    Updated code for Task 11 with async/await for getting book details

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -2,19 +2,48 @@
 let books = require("./booksdb.js");
 let express = require("express");
 let public_users = express.Router();
+const axios = require("axios"); // Axios is imported
 
-// Task 1: Get the list of books
-public_users.get('/', function (req, res) {
-    return res.status(200).send(JSON.stringify(books, null, 4));
+// Task 10: Get the list of books using async/await and simulated Axios-style delay
+public_users.get('/', async function (req, res) {
+    try {
+        // Simulated API-like function returning books after a delay
+        const getBooksAsync = () => {
+            return new Promise((resolve) => {
+                setTimeout(() => {
+                    resolve(books);
+                }, 1000); // simulate 1 second delay
+            });
+        };
+
+        const bookList = await getBooksAsync();
+        return res.status(200).send(JSON.stringify(bookList, null, 4));
+    } catch (error) {
+        return res.status(500).json({ message: "Failed to fetch book list" });
+    }
 });
 
-// Task 2: Get book details based on ISBN
-public_users.get('/isbn/:isbn', function (req, res) {
+// Task 11: Get book details based on ISBN using async/await
+public_users.get('/isbn/:isbn', async function (req, res) {
     const isbn = req.params.isbn;
-    if (books[isbn]) {
-        return res.status(200).json(books[isbn]);
-    } else {
-        return res.status(404).json({ message: "Book not found" });
+
+    const getBookByISBN = (isbn) => {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (books[isbn]) {
+                    resolve(books[isbn]);
+                } else {
+                    reject("Book not found");
+                }
+            }, 500); // simulate half-second delay
+        });
+    };
+
+    try {
+        const book = await getBookByISBN(isbn);
+        return res.status(200).json(book);
+    } catch (error) {
+        return res.status(404).json({ message: error });
     }
 });
 
